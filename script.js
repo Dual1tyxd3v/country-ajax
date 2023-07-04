5e48d29351743d466ca4fc470675d7bf777f875d
B3rzxd3v <d3vilc0re@gmail.com>
2023-07-04 19:32:48 +0700

    Add get data function with promises

--- script.js
+++ script.js
@@ -5,29 +5,39 @@ const countriesContainer = document.querySelector('.countries');
 const URL = 'https://restcountries.com/v2/name/';
 
 ///////////////////////////////////////
+const renderCountry = (data) => {
+  const html = `
+  <article class="country">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(+data.population / 1_000_000).toFixed(2)}</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
 const getCountryData = (country) => {
   const request = new XMLHttpRequest();
   request.open('GET', URL + country);
   request.send();
   request.addEventListener('load', function () {
     const [data] = JSON.parse(this.responseText);
-    console.log(data)
-    const html = `
-    <article class="country">
-      <img class="country__img" src="${data.flag}" />
-      <div class="country__data">
-        <h3 class="country__name">${data.name}</h3>
-        <h4 class="country__region">${data.region}</h4>
-        <p class="country__row"><span>👫</span>${(+data.population / 1_000_000).toFixed(2)}</p>
-        <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-        <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-      </div>
-    </article>`;
-
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+    renderCountry(data);
   });
 };
 
+const getCountryDataFetch = (country) => {
+  fetch(URL + country)
+    .then(res => res.json())
+    .then(res => renderCountry(res[0]));
+};
+
+getCountryDataFetch('usa');
 getCountryData('russia');
 
